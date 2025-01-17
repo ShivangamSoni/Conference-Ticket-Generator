2c3b59026843eb15d8d7a95e81f63be6ec09a0d5
Shivangam Soni <shivangamsoni99@gmail.com>
2025-01-17 10:59:35 +0530

    Init: Conference Ticket Generator App

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <main className="w-screen h-screen bg-slate-300">
+            <p className="text-emerald-400">App</p>
+        </main>
+    );
+}
