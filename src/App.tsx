08fadb5058f0d96dfe8d317ed6000a66535d10c1
Shivangam Soni <shivangamsoni99@gmail.com>
2025-01-17 12:17:24 +0530

    Add: Background & Main Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { LogoFullIcon } from "./assets";
+
 export default function App() {
     return (
-        <main className="w-screen h-screen bg-slate-300 font-primary bg-image">
-            <p className="text-emerald-700 text-8xl">App</p>
+        <main className="w-screen h-screen font-primary text-neutral-0 main__bg flex flex-col items-center py-12 px-2 sm:py-16 sm:px-4 lg:py-20 lg:px-8">
+            <header>
+                <h1 className="sr-only">Coding Conf</h1>
+                <LogoFullIcon aria-hidden className="" />
+            </header>
+            Content
         </main>
     );
 }
