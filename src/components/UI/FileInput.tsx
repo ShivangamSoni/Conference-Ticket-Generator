f7d7334fc5701e0008248c85eb419005fb8b6a08
Shivangam Soni <shivangamsoni99@gmail.com>
2025-01-20 19:40:52 +0530

    Update: Add Remove & Change Buttons

--- src/components/UI/FileInput.tsx
+++ src/components/UI/FileInput.tsx
@@ -1,5 +1,6 @@
-import { ComponentProps, useId } from "react";
+import { ComponentProps, useId, useState } from "react";
 import { InfoIcon, UploadIcon } from "../../assets";
+import Button from "./Button";
 
 type PropsType = {
     label: string;
@@ -19,6 +20,10 @@ export default function FileInput({
 }: PropsType) {
     const id = useId();
     const fileInputId = `${id}-file`;
+    const [isFileDrag, setIsFileDrag] = useState(false);
+
+    const fileContainer =
+        "group flex flex-col items-center justify-center gap-4 text-xl md:text-2xl text-neutral-300 bg-white bg-opacity-10 cursor-pointer p-4 md:p-8 border-2 border-neutral-500 border-dashed rounded-xl outline outline-2 outline-offset-[3px] outline-transparent transition-all duration-200";
 
     return (
         <div>
@@ -31,38 +36,57 @@ export default function FileInput({
                     type="file"
                     {...rest}
                 />
-                <label
-                    htmlFor={fileInputId}
-                    tabIndex={1}
-                    className="group flex flex-col items-center justify-center gap-4 text-xl md:text-2xl text-neutral-300 bg-white bg-opacity-10 cursor-pointer p-4 md:p-8 border-2 border-neutral-500 border-dashed rounded-xl outline outline-2 outline-offset-[3px] outline-transparent transition-all duration-200 hover:bg-opacity-50 focus:outline-neutral-500 focus-visible:outline-neutral-500"
-                    onDragOver={(e) => {
-                        e.stopPropagation();
-                        e.preventDefault();
-                    }}
-                    onDrop={(e) => {
-                        e.preventDefault();
-                        if (e.dataTransfer.items) {
-                            const item = e.dataTransfer.items[0];
-                            if (item.kind === "file") {
-                                const file = item.getAsFile();
-                                onFileDrop(file);
+
+                {!file ? (
+                    <label
+                        htmlFor={fileInputId}
+                        tabIndex={1}
+                        className={`${fileContainer} hover:bg-opacity-50 focus:outline-neutral-500 focus-visible:outline-neutral-500`}
+                        onDragOver={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            setIsFileDrag(true);
+                        }}
+                        onDragEnd={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            setIsFileDrag(false);
+                        }}
+                        onDrop={(e) => {
+                            e.preventDefault();
+                            if (e.dataTransfer.items) {
+                                const item = e.dataTransfer.items[0];
+                                if (item.kind === "file") {
+                                    const file = item.getAsFile();
+                                    onFileDrop(file);
+                                }
                             }
-                        }
-                    }}
-                >
-                    {!file ? (
-                        <>
-                            <UploadIcon className="w-10 h-10 p-1 border rounded-xl shadow-md bg-white bg-opacity-20 group-hover:bg-opacity-50" />
-                            Drag and drop or click to upload
-                        </>
-                    ) : (
+                        }}
+                    >
+                        <UploadIcon
+                            className={`w-10 h-10 p-1 border rounded-xl shadow-md bg-white bg-opacity-20 group-hover:bg-opacity-50 ${
+                                isFileDrag ? "animate-bounce" : ""
+                            }`}
+                        />
+                        Drag and drop or click to upload
+                    </label>
+                ) : (
+                    <div className={fileContainer}>
                         <img
                             src={URL.createObjectURL(file)}
                             className="w-32 h-32 aspect-square rounded-xl object-cover overflow-hidden"
                             alt=""
                         />
-                    )}
-                </label>
+                        <div className="flex items-center justify-center gap-2 md:gap-4">
+                            <Button variant="ghost" type="button">
+                                Remove Image
+                            </Button>
+                            <Button variant="ghost" type="button">
+                                Change Image
+                            </Button>
+                        </div>
+                    </div>
+                )}
 
                 {info && (
                     <span className="mt-1 flex items-center justify-start gap-1 text-neutral-500 text-sm">
