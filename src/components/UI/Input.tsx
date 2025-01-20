648a2911c34a6515f8fcaa0f90fd5e0b62bb9de7
Shivangam Soni <shivangamsoni99@gmail.com>
2025-01-20 20:31:54 +0530

    Update: Forward Ref

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 
 import { InfoIcon } from "../../assets";
 
@@ -8,11 +8,15 @@ type PropsType =
           error: null | string;
       } & Omit<ComponentProps<"input">, "className">;
 
-export default function Input({ label, error, ...rest }: PropsType) {
+export default forwardRef<HTMLInputElement, PropsType>(function Input(
+    { label, error, ...rest },
+    ref
+) {
     return (
         <label className="grid gap-2">
             <span className="text-lg">{label}</span>
             <input
+                ref={ref}
                 {...rest}
                 className="cursor-pointer text-lg px-4 py-2.5 bg-white bg-opacity-10 rounded-xl border-2 border-neutral-500 outline outline-2 outline-offset-[3px] outline-transparent transition-all duration-200 hover:bg-opacity-50 focus:outline-neutral-500 focus-visible:outline-neutral-500"
             />
@@ -23,4 +27,4 @@ export default function Input({ label, error, ...rest }: PropsType) {
             )}
         </label>
     );
-}
+});
