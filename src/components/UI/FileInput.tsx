33c2834ecb095abb7a126ecc86f2dc1bf310b01c
Shivangam Soni <shivangamsoni99@gmail.com>
2025-01-20 19:09:15 +0530

    Update: FileInput Drag & Drop Functionality

--- src/components/UI/FileInput.tsx
+++ src/components/UI/FileInput.tsx
@@ -6,6 +6,7 @@ type PropsType = {
     error: null | string;
     info: null | string;
     file: File | null;
+    onFileDrop: (file: File | null) => void;
 } & Omit<ComponentProps<"input">, "type" & "className">;
 
 export default function FileInput({
@@ -13,6 +14,7 @@ export default function FileInput({
     file,
     error,
     info,
+    onFileDrop,
     ...rest
 }: PropsType) {
     const id = useId();
@@ -20,28 +22,47 @@ export default function FileInput({
 
     return (
         <div>
-            <input id={fileInputId} className="hidden" type="file" {...rest} />
             <div className="grid gap-2">
                 <span className="text-lg">{label}</span>
 
-                {!file ? (
-                    <label
-                        htmlFor={fileInputId}
-                        tabIndex={1}
-                        className="group flex flex-col items-center justify-center gap-4 text-xl md:text-2xl text-neutral-300 bg-white bg-opacity-10 cursor-pointer p-4 md:p-8 border-2 border-neutral-500 border-dashed rounded-xl outline outline-2 outline-offset-[3px] outline-transparent transition-all duration-200 hover:bg-opacity-50 focus:outline-neutral-500 focus-visible:outline-neutral-500"
-                    >
-                        <UploadIcon className="w-10 h-10 p-1 border rounded-xl shadow-md bg-white bg-opacity-20 group-hover:bg-opacity-50" />
-                        Drag and drop or click to upload
-                    </label>
-                ) : (
-                    <div className="group flex flex-col items-center justify-center gap-4 text-xl md:text-2xl text-neutral-300 bg-white bg-opacity-10 cursor-pointer p-4 md:p-8 border-2 border-neutral-500 border-dashed rounded-xl outline outline-2 outline-offset-[3px] outline-transparent transition-all duration-200 hover:bg-opacity-50 focus:outline-neutral-500 focus-visible:outline-neutral-500">
+                <input
+                    id={fileInputId}
+                    className="hidden"
+                    type="file"
+                    {...rest}
+                />
+                <label
+                    htmlFor={fileInputId}
+                    tabIndex={1}
+                    className="group flex flex-col items-center justify-center gap-4 text-xl md:text-2xl text-neutral-300 bg-white bg-opacity-10 cursor-pointer p-4 md:p-8 border-2 border-neutral-500 border-dashed rounded-xl outline outline-2 outline-offset-[3px] outline-transparent transition-all duration-200 hover:bg-opacity-50 focus:outline-neutral-500 focus-visible:outline-neutral-500"
+                    onDragOver={(e) => {
+                        e.stopPropagation();
+                        e.preventDefault();
+                    }}
+                    onDrop={(e) => {
+                        e.preventDefault();
+                        if (e.dataTransfer.items) {
+                            const item = e.dataTransfer.items[0];
+                            if (item.kind === "file") {
+                                const file = item.getAsFile();
+                                onFileDrop(file);
+                            }
+                        }
+                    }}
+                >
+                    {!file ? (
+                        <>
+                            <UploadIcon className="w-10 h-10 p-1 border rounded-xl shadow-md bg-white bg-opacity-20 group-hover:bg-opacity-50" />
+                            Drag and drop or click to upload
+                        </>
+                    ) : (
                         <img
                             src={URL.createObjectURL(file)}
                             className="w-32 h-32 aspect-square rounded-xl object-cover overflow-hidden"
                             alt=""
                         />
-                    </div>
-                )}
+                    )}
+                </label>
 
                 {info && (
                     <span className="mt-1 flex items-center justify-start gap-1 text-neutral-500 text-sm">
