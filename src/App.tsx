d8f6efea84645dc5b531abc6e0a7c5b3d85669f1
Shivangam Soni <shivangamsoni99@gmail.com>
2025-01-22 19:29:31 +0530

    Fix: App Height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { LogoFullIcon } from "./assets";
 
 export default function App() {
     return (
-        <main className="w-screen h-screen font-primary text-neutral-0 main__bg flex flex-col items-center py-12 gap-12 px-2 sm:py-16 sm:px-4 sm:gap-16 lg:py-20 lg:px-8 lg:gap-20">
+        <main className="w-screen min-h-screen font-primary text-neutral-0 main__bg flex flex-col items-center py-12 gap-12 px-2 sm:py-16 sm:px-4 sm:gap-16 lg:py-20 lg:px-8 lg:gap-20">
             <header>
                 <h1 className="sr-only">Coding Conf</h1>
                 <LogoFullIcon aria-hidden className="" />
